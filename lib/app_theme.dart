7d25796299d697024e15749a3f8ac1dcf20cd4a6
bijan <bijan.sellahi@vilua.com>
2020-04-13 13:54:17 +0200

    add dark theme

--- lib/app_theme.dart
+++ lib/app_theme.dart
@@ -10,6 +10,37 @@ class AppTheme {
   static const Color _lightSecondaryColor = Colors.green;
   static const Color _lightOnPrimaryColor = Colors.black;
 
+  static const Color _darkPrimaryColor = Colors.black;
+  static const Color _darkPrimaryVariantColor = Color(0xFF404041); // Scaffold background
+  static const Color _darkSecondaryColor = Colors.green;
+  static const Color _darkOnPrimaryColor = Colors.white;
+
+  static final TextStyle _lightScreenHeadingTextStyle = TextStyle(fontSize: 48.0, color: _lightOnPrimaryColor);
+  static final TextStyle _lightScreenMenuTextStyle = TextStyle(fontSize: 20.0, color: _lightOnPrimaryColor);
+  static final TextStyle _lightScreenSingleCardTextStyle = TextStyle(fontSize: 180.0, color: _lightOnPrimaryColor);
+  static final TextStyle _lightGridViewCardTextStyle = TextStyle(fontSize: 34.0, color: _lightOnPrimaryColor);
+
+  static final TextStyle _darkScreenHeadingTextStyle =
+      _lightScreenHeadingTextStyle.copyWith(color: _darkOnPrimaryColor);
+  static final TextStyle _darkScreenMenuTextStyle = _lightScreenMenuTextStyle.copyWith(color: _darkOnPrimaryColor);
+  static final TextStyle _darkScreenSingleCardTextStyle =
+      _lightScreenSingleCardTextStyle.copyWith(color: _darkOnPrimaryColor);
+  static final TextStyle _darkGridViewCardTextStyle = _lightGridViewCardTextStyle.copyWith(color: _darkOnPrimaryColor);
+
+  static final TextTheme _lightTextTheme = TextTheme(
+    headline: _lightScreenHeadingTextStyle,
+    body1: _lightScreenMenuTextStyle,
+    display1: _lightScreenSingleCardTextStyle,
+    display2: _lightGridViewCardTextStyle,
+  );
+
+  static final TextTheme _darkTextTheme = TextTheme(
+    headline: _darkScreenHeadingTextStyle,
+    body1: _darkScreenMenuTextStyle,
+    display1: _darkScreenSingleCardTextStyle,
+    display2: _darkGridViewCardTextStyle,
+  );
+
   static final ThemeData lightTheme = ThemeData(
     scaffoldBackgroundColor: _lightPrimaryVariantColor,
     appBarTheme: AppBarTheme(
@@ -39,15 +70,22 @@ class AppTheme {
     textTheme: _lightTextTheme,
   );
 
-  static final TextStyle _lightScreenHeadingTextStyle = TextStyle(fontSize: 48.0, color: _lightOnPrimaryColor);
-  static final TextStyle _lightScreenMenuTextStyle = TextStyle(fontSize: 20.0, color: _lightOnPrimaryColor);
-  static final TextStyle _lightScreenSingleCardTextStyle = TextStyle(fontSize: 180.0, color: _lightOnPrimaryColor);
-  static final TextStyle _lightGridViewCardTextStyle = TextStyle(fontSize: 34.0, color: _lightOnPrimaryColor);
-
-  static final TextTheme _lightTextTheme = TextTheme(
-    headline: _lightScreenHeadingTextStyle,
-    body1: _lightScreenMenuTextStyle,
-    display1: _lightScreenSingleCardTextStyle,
-    display2: _lightGridViewCardTextStyle,
+  static final ThemeData darkTheme = ThemeData(
+    scaffoldBackgroundColor: _darkPrimaryVariantColor,
+    appBarTheme: AppBarTheme(
+      color: Colors.transparent,
+      iconTheme: IconThemeData(color: _darkOnPrimaryColor),
+      textTheme: TextTheme(
+        title: TextStyle(fontSize: 24, color: _darkOnPrimaryColor),
+      ),
+    ),
+    cardColor: _darkPrimaryVariantColor,
+    colorScheme: ColorScheme.light(
+      primary: _darkPrimaryColor,
+      primaryVariant: _darkPrimaryVariantColor,
+      secondary: _darkSecondaryColor,
+    ),
+    iconTheme: IconThemeData(color: _iconColor),
+    textTheme: _darkTextTheme,
   );
 }
