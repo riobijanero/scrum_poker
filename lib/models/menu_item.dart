d16ed37fbab8191938794f6254c1e9436139ce07
bijan <bijan.sellahi@vilua.com>
2020-05-12 08:37:46 +0200

    move menu item to models folder

---
+++ lib/models/menu_item.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class MenuItem {
+  final String menuItemTitle;
+  bool isSelected;
+  final IconData menuItemicon;
+
+  MenuItem({this.menuItemTitle, this.isSelected, this.menuItemicon});
+}
