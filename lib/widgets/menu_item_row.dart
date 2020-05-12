c0fac1084d3991297f3e0b9ec44d34a6e1a58823
bijan <bijan.sellahi@vilua.com>
2020-05-12 08:37:59 +0200

    create menu item row widget

---
+++ lib/widgets/menu_item_row.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import '../models/menu_item.dart';
+
+class MenutItemRow extends StatelessWidget {
+  final MenuItem menuItem;
+  final void Function() onPressed;
+  const MenutItemRow({this.menuItem, this.onPressed});
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onPressed,
+      child: Container(
+        padding: EdgeInsets.symmetric(vertical: 10),
+        child: Row(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(right: 8.0),
+              child: Icon(menuItem.menuItemicon),
+            ),
+            Text(menuItem.menuItemTitle,
+                style: menuItem.isSelected ? TextStyle(color: Theme.of(context).iconTheme.color) : null),
+          ],
+        ),
+      ),
+    );
+  }
+}
