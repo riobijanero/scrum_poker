5905e7a959146b4eedcf3c280e3a18a4aebef53b
bijan <bijan.sellahi@vilua.com>
2020-05-28 17:39:25 +0200

    extend EstimationValue with isImage bool

--- lib/models/estimation_value.dart
+++ lib/models/estimation_value.dart
@@ -1,6 +1,7 @@
 class EstimationValue {
   final dynamic value;
   final String description;
+  final bool isImage;
 
-  const EstimationValue({this.value, this.description});
+  const EstimationValue({this.value, this.description, this.isImage = false});
 }
