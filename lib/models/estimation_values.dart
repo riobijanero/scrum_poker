bddebea9d79876edc04ae099cf7005b8c4a3bc16
bijan <bijan.sellahi@vilua.com>
2020-05-28 17:39:50 +0200

    extend data source with images

--- lib/models/estimation_values.dart
+++ lib/models/estimation_values.dart
@@ -2,31 +2,43 @@ import './estimation_value.dart';
 
 class EstimationValues {
   static List<EstimationValue> standardList = [
-    EstimationValue(value: 1.toString(), description: 'test description'),
-    EstimationValue(value: 2.toString(), description: 'test description'),
-    EstimationValue(value: 3.toString(), description: 'test description'),
-    EstimationValue(value: 5.toString(), description: 'test description'),
-    EstimationValue(value: 8.toString(), description: 'test description'),
-    EstimationValue(value: 13.toString(), description: 'test description'),
-    EstimationValue(value: 20.toString(), description: 'test description'),
-    EstimationValue(value: 40.toString(), description: 'test description'),
-    EstimationValue(value: 100.toString(), description: 'test description'),
+    EstimationValue(value: '1', description: 'test description'),
+    EstimationValue(value: '2', description: 'test description'),
+    EstimationValue(value: '3', description: 'test description'),
+    EstimationValue(value: '5', description: 'test description'),
+    EstimationValue(value: '8', description: 'test description'),
+    EstimationValue(value: '13', description: 'test description'),
+    EstimationValue(value: '20', description: 'test description'),
+    EstimationValue(value: '40', description: 'test description'),
+    EstimationValue(value: '100', description: 'test description'),
     EstimationValue(value: '∞', description: 'test description'),
-    EstimationValue(value: '?', description: 'test description')
+    EstimationValue(value: '?', description: 'test description'),
+    EstimationValue(
+        value: 'assets/images/nathan-dumlao-nBJHO6wmRWw-unsplash.jpg', description: 'test description', isImage: true),
+    EstimationValue(
+        value: 'assets/images/jeremy-yap-jn-HaGWe4yw-unsplash.jpg', description: 'test description', isImage: true),
+    EstimationValue(
+        value: 'assets/images/olivier-collet-935jH5zSqD8-unsplash.png', description: 'test description', isImage: true),
   ];
 
   static List<EstimationValue> fibonacciList = [
-    EstimationValue(value: 1.toString(), description: 'test description'),
-    EstimationValue(value: 2.toString(), description: 'test description'),
-    EstimationValue(value: 3.toString(), description: 'test description'),
-    EstimationValue(value: 5.toString(), description: 'test description'),
-    EstimationValue(value: 8.toString(), description: 'test description'),
-    EstimationValue(value: 13.toString(), description: 'test description'),
-    EstimationValue(value: 21.toString(), description: 'test description'),
-    EstimationValue(value: 34.toString(), description: 'test description'),
-    EstimationValue(value: 55.toString(), description: 'test description'),
+    EstimationValue(value: '1', description: 'test description'),
+    EstimationValue(value: '2', description: 'test description'),
+    EstimationValue(value: '3', description: 'test description'),
+    EstimationValue(value: '5', description: 'test description'),
+    EstimationValue(value: '8', description: 'test description'),
+    EstimationValue(value: '13', description: 'test description'),
+    EstimationValue(value: '21', description: 'test description'),
+    EstimationValue(value: '34', description: 'test description'),
+    EstimationValue(value: '55', description: 'test description'),
     EstimationValue(value: '∞', description: 'test description'),
     EstimationValue(value: '?', description: 'test description'),
+    EstimationValue(
+        value: 'assets/images/nathan-dumlao-nBJHO6wmRWw-unsplash.jpg', description: 'test description', isImage: true),
+    EstimationValue(
+        value: 'assets/images/jeremy-yap-jn-HaGWe4yw-unsplash.jpg', description: 'test description', isImage: true),
+    EstimationValue(
+        value: 'assets/images/olivier-collet-935jH5zSqD8-unsplash.png', description: 'test description', isImage: true),
   ];
 
   static const List<EstimationValue> tShirtList = [
@@ -38,5 +50,11 @@ class EstimationValues {
     EstimationValue(value: 'XXL', description: 'test description'),
     EstimationValue(value: '∞', description: 'test description'),
     EstimationValue(value: '?', description: 'test description'),
+    EstimationValue(
+        value: 'assets/images/nathan-dumlao-nBJHO6wmRWw-unsplash.jpg', description: 'test description', isImage: true),
+    EstimationValue(
+        value: 'assets/images/jeremy-yap-jn-HaGWe4yw-unsplash.jpg', description: 'test description', isImage: true),
+    EstimationValue(
+        value: 'assets/images/olivier-collet-935jH5zSqD8-unsplash.png', description: 'test description', isImage: true),
   ];
 }
