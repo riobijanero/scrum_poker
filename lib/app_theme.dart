0fc461905a59f6f5e25c1c115271775a34131f9d
Bijan Sellahi <bisevilua.net@Bijans-MacBook-Pro.local>
2020-07-09 16:58:50 +0200

    modify apptheme

--- lib/app_theme.dart
+++ lib/app_theme.dart
@@ -22,7 +22,7 @@ class AppTheme {
   }
 
   static Color _lightIconColor = Colors.blueAccent.shade200;
-  static Color _darkIconColor = Colors.yellowAccent;
+  static Color _darkIconColor = Colors.blue;
 
   static Color _lightPrimaryColor = Color(0XFFF5F5F5);
   static const Color _lightPrimaryVariantColor =
@@ -36,6 +36,13 @@ class AppTheme {
   static const Color _darkSecondaryColor = Colors.white70;
   static const Color _darkOnPrimaryColor = Colors.white;
 
+  static const Color _customPurple = Color(0x777a6fff);
+  static const Color _customLightBlue = Color(0x666da7ff);
+
+  static const Color _lightCardColor = Colors.white;
+
+  static const Color _darkCardColor = Color(0xFF404041);
+
   static final TextStyle _lightScreenHeadingTextStyle =
       TextStyle(fontSize: 48.0, color: _lightOnPrimaryColor);
   static final TextStyle _lightScreenMenuTextStyle =
@@ -47,7 +54,7 @@ class AppTheme {
   static final TextStyle _lightGridViewCardTextStyle =
       TextStyle(fontSize: 34.0, color: _lightOnPrimaryColor);
   static final TextStyle _lightCardRowViewCardTextStyle =
-      TextStyle(fontSize: 20.0, color: _lightIconColor);
+      TextStyle(fontSize: 21.0, color: _lightIconColor);
 
   static final TextStyle _darkScreenHeadingTextStyle =
       _lightScreenHeadingTextStyle.copyWith(color: _darkOnPrimaryColor);
@@ -58,26 +65,26 @@ class AppTheme {
   static final TextStyle _darkGridViewCardTextStyle =
       _lightGridViewCardTextStyle.copyWith(color: _darkOnPrimaryColor);
   static final TextStyle _darkCardRowViewCardTextStyle =
-      _lightCardRowViewCardTextStyle.copyWith(color: _darkIconColor);
+      _lightCardRowViewCardTextStyle.copyWith(color: Colors.white);
   static final TextStyle _darkScreenDescriptionTextStyle =
       _lightScreenDescriptionTextStyle.copyWith(color: _darkOnPrimaryColor);
 
   static final TextTheme _lightTextTheme = TextTheme(
-    headline5: _lightScreenHeadingTextStyle,
-    bodyText2: _lightScreenMenuTextStyle,
-    bodyText1: _lightScreenDescriptionTextStyle,
-    headline4: _lightScreenSingleCardTextStyle,
     headline3: _lightGridViewCardTextStyle,
+    headline4: _lightScreenSingleCardTextStyle,
+    headline5: _lightScreenHeadingTextStyle,
     headline6: _lightCardRowViewCardTextStyle,
+    bodyText1: _lightScreenDescriptionTextStyle,
+    bodyText2: _lightScreenMenuTextStyle,
   );
 
   static final TextTheme _darkTextTheme = TextTheme(
-    headline5: _darkScreenHeadingTextStyle,
-    bodyText2: _darkScreenMenuTextStyle,
-    headline4: _darkScreenSingleCardTextStyle,
     headline3: _darkGridViewCardTextStyle,
+    headline4: _darkScreenSingleCardTextStyle,
+    headline5: _darkScreenHeadingTextStyle,
     headline6: _darkCardRowViewCardTextStyle,
     bodyText1: _darkScreenDescriptionTextStyle,
+    bodyText2: _darkScreenMenuTextStyle,
   );
 
   static final ThemeData lightTheme = ThemeData(
@@ -89,25 +96,19 @@ class AppTheme {
         headline6: TextStyle(fontSize: 24, color: _lightOnPrimaryColor),
       ),
     ),
-    cardColor: _lightPrimaryColor,
+    cardColor: _lightCardColor,
     colorScheme: ColorScheme.light(
       primary: _lightPrimaryColor,
       primaryVariant: _lightPrimaryVariantColor,
       secondary: _lightSecondaryColor,
       onPrimary: _lightOnPrimaryColor,
       surface: Colors.white,
-      // secondaryVariant: null,
-      // background: null,
-      // error: null,
-      // onSecondary: null,
-      // onSurface: null,
-      // onBackground: null,
-      // onError: null,
-      // brightness: null,
     ),
     iconTheme: IconThemeData(color: _lightIconColor),
     toggleableActiveColor: _lightOnPrimaryColor,
     textTheme: _lightTextTheme,
+    accentColor: Colors.blue,
+    splashColor: _customLightBlue,
   );
 
   static final ThemeData darkTheme = ThemeData(
@@ -119,7 +120,7 @@ class AppTheme {
         headline6: TextStyle(fontSize: 24, color: _darkOnPrimaryColor),
       ),
     ),
-    cardColor: _darkPrimaryVariantColor,
+    cardColor: _darkCardColor,
     colorScheme: ColorScheme.dark(
       primary: _darkPrimaryColor,
       primaryVariant: _darkPrimaryVariantColor,
@@ -127,8 +128,10 @@ class AppTheme {
       onPrimary: _darkOnPrimaryColor,
       surface: Colors.black54,
     ),
-    iconTheme: IconThemeData(color: _darkIconColor),
+    iconTheme: IconThemeData(color: Colors.blueAccent.shade200),
     toggleableActiveColor: _darkIconColor,
     textTheme: _darkTextTheme,
+    accentColor: _customPurple,
+    splashColor: Colors.blue,
   );
 }
