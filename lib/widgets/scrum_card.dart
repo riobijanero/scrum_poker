17abd76f9e3c90d09f482c2f9aa4c7da3dc7f95a
Bijan Sellahi <bisevilua.net@Bijans-MacBook-Pro.local>
2020-07-09 16:59:54 +0200

    refactor color scheme of scrum card

--- lib/widgets/scrum_card.dart
+++ lib/widgets/scrum_card.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../app_state.dart';
 import '../models/estimation_value.dart';
 
 class ScrumCard extends StatelessWidget {
@@ -14,15 +16,27 @@ class ScrumCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    bool isDarkMode = Provider.of<AppState>(context).isDarkMode;
     return AspectRatio(
       aspectRatio: 3.5 / 5.2,
-      child: Card(
-        shape: RoundedRectangleBorder(
-          side: BorderSide(width: 0.2),
+      child: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              stops: [0.4, 1],
+              colors: isDarkMode
+                  ? [
+                      Theme.of(context).accentColor,
+                      Theme.of(context).splashColor,
+                    ]
+                  : [Theme.of(context).cardColor, Theme.of(context).cardColor]),
           borderRadius: BorderRadius.circular(borderRadius),
+          boxShadow: [
+            BoxShadow(color: Colors.black54, blurRadius: isDarkMode ? 4 : 8)
+          ],
         ),
         margin: EdgeInsets.all(30),
-        elevation: 10,
         child: estimationValue.isImage
             ? ClipRRect(
                 borderRadius: BorderRadius.circular(borderRadius),
@@ -34,7 +48,9 @@ class ScrumCard extends StatelessWidget {
             : Center(
                 child: Text(
                 estimationValue.value,
-                style: isSelected ? Theme.of(context).textTheme.display1 : Theme.of(context).textTheme.display2,
+                style: isSelected
+                    ? Theme.of(context).textTheme.headline4
+                    : Theme.of(context).textTheme.headline3,
               )),
       ),
     );
