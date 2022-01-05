cd7c5f3bda4966f93268e9df433ab70961653262
bijan <riobijanero@gmail.com>
2022-01-05 11:59:04 +0100

    Correct Scrum Value Explanations

--- lib/models/estimation_values.dart
+++ lib/models/estimation_values.dart
@@ -3,77 +3,59 @@ import './estimation_value.dart';
 class EstimationValues {
   static List<EstimationValue> standardList = [
     EstimationValue(value: '1', description: 'Changing a color'),
-    EstimationValue(value: '2', description: 'test description'),
-    EstimationValue(value: '3', description: 'test description'),
-    EstimationValue(value: '5', description: 'test description'),
-    EstimationValue(value: '8', description: 'test description'),
-    EstimationValue(value: '13', description: 'test description'),
-    EstimationValue(value: '20', description: 'test description'),
-    EstimationValue(value: '40', description: 'test description'),
-    EstimationValue(value: '100', description: 'test description'),
-    EstimationValue(
-        value: '∞', description: 'yeah, we need more Developers for that'),
-    EstimationValue(value: '?', description: 'Da Fuck??'), // EstimationValue(
+    EstimationValue(value: '2', description: 'Changing severeral Vars'),
+    EstimationValue(value: '3', description: 'Small subfeature'),
+    EstimationValue(value: '5', description: 'Subticket Maximum'),
+    EstimationValue(value: '8', description: 'Takes a Full sprint'),
+    EstimationValue(value: '13', description: 'Needs to be split up'),
+    EstimationValue(value: '20', description: 'This is an epic'),
+    EstimationValue(value: '40', description: 'This also an epic but more'),
+    EstimationValue(value: '100', description: 'We need more Devs'),
+    EstimationValue(value: '∞', description: 'yeah, we need more Developers for that'),
+    EstimationValue(value: '?', description: 'What??'), // EstimationValue(
     //     value: 'assets/images/nathan-dumlao-nBJHO6wmRWw-unsplash.jpg',
     //     description: 'test description',
     //     isImage: true),
     EstimationValue(
-        value: 'assets/images/Fuer_kleine_Koenigstiger_5pt.png',
-        description: 'I need to take whiz',
-        isImage: true),
-    EstimationValue(
-        value: 'assets/images/Ok-but-first-coffee_5pt.png',
-        description: 'I need a coffee',
-        isImage: true),
+        value: 'assets/images/Fuer_kleine_Koenigstiger_5pt.png', description: 'I need to take whiz', isImage: true),
+    EstimationValue(value: 'assets/images/Ok-but-first-coffee_5pt.png', description: 'I need a coffee', isImage: true),
   ];
 
   static List<EstimationValue> fibonacciList = [
     EstimationValue(value: '1', description: 'Changing a color'),
-    EstimationValue(value: '2', description: 'test description'),
-    EstimationValue(value: '3', description: 'test description'),
-    EstimationValue(value: '5', description: 'test description'),
-    EstimationValue(value: '8', description: 'test description'),
-    EstimationValue(value: '13', description: 'test description'),
-    EstimationValue(value: '21', description: 'test description'),
-    EstimationValue(value: '34', description: 'test description'),
-    EstimationValue(value: '55', description: 'test description'),
-    EstimationValue(
-        value: '∞', description: 'yeah, we need more Developers for that'),
-    EstimationValue(value: '?', description: 'Da Fuck??'), // EstimationValue(
+    EstimationValue(value: '2', description: 'Changing severeral Vars'),
+    EstimationValue(value: '3', description: 'Small subfeature'),
+    EstimationValue(value: '5', description: 'Subticket Maximum'),
+    EstimationValue(value: '8', description: 'Takes a Full sprint'),
+    EstimationValue(value: '13', description: 'Needs to be split up'),
+    EstimationValue(value: '21', description: 'This is an epic'),
+    EstimationValue(value: '34', description: 'This also an epic but more'),
+    EstimationValue(value: '55', description: 'We need more Devs'),
+    EstimationValue(value: '∞', description: 'yeah, we really need more Developers for that'),
+    EstimationValue(value: '?', description: 'What??'), // EstimationValue(
     //     value: 'assets/images/nathan-dumlao-nBJHO6wmRWw-unsplash.jpg',
     //     description: 'test description',
     //     isImage: true),
     EstimationValue(
-        value: 'assets/images/Fuer_kleine_Koenigstiger_5pt.png',
-        description: 'I need to take whiz',
-        isImage: true),
-    EstimationValue(
-        value: 'assets/images/Ok-but-first-coffee_5pt.png',
-        description: 'I need a coffee',
-        isImage: true),
+        value: 'assets/images/Fuer_kleine_Koenigstiger_5pt.png', description: 'I need to take whiz', isImage: true),
+    EstimationValue(value: 'assets/images/Ok-but-first-coffee_5pt.png', description: 'I need a coffee', isImage: true),
   ];
 
   static const List<EstimationValue> tShirtList = [
-    EstimationValue(value: 'XS', description: 'test description'),
-    EstimationValue(value: 'S', description: 'test description'),
-    EstimationValue(value: 'M', description: 'test description'),
-    EstimationValue(value: 'L', description: 'test description'),
-    EstimationValue(value: 'XL', description: 'test description'),
-    EstimationValue(value: 'XXL', description: 'test description'),
-    EstimationValue(
-        value: '∞', description: 'yeah, we need more Developers for that'),
+    EstimationValue(value: 'XS', description: 'Changing a color'),
+    EstimationValue(value: 'S', description: 'Small subfeature'),
+    EstimationValue(value: 'M', description: 'Subticket Maximum'),
+    EstimationValue(value: 'L', description: 'Takes a Full sprint'),
+    EstimationValue(value: 'XL', description: 'This is an epic'),
+    EstimationValue(value: 'XXL', description: 'This also an epic but more'),
+    EstimationValue(value: '∞', description: 'yeah, we need more Developers for that'),
     EstimationValue(value: '?', description: 'Da Fuck??'),
     // EstimationValue(
     //     value: 'assets/images/nathan-dumlao-nBJHO6wmRWw-unsplash.jpg',
     //     description: 'test description',
     //     isImage: true),
     EstimationValue(
-        value: 'assets/images/Fuer_kleine_Koenigstiger_5pt.png',
-        description: 'I need to take whiz',
-        isImage: true),
-    EstimationValue(
-        value: 'assets/images/Ok-but-first-coffee_5pt.png',
-        description: 'I need a coffee',
-        isImage: true),
+        value: 'assets/images/Fuer_kleine_Koenigstiger_5pt.png', description: 'I need to take whiz', isImage: true),
+    EstimationValue(value: 'assets/images/Ok-but-first-coffee_5pt.png', description: 'I need a coffee', isImage: true),
   ];
 }
